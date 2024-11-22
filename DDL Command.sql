d21503d3665b8b59d424151a3250574120c749bf
Aishwarya_09 <157670328+AishwaryaWAnkhade01@users.noreply.github.com>
2024-11-22 13:22:26 +0530

    Add files via upload
    Data Definition Language

---
+++ DDL Command.sql
@@ -0,0 +1,29 @@
+--- -Data Definition Language--  
+-- CREATE-- 
+Create database Employee_Info;
+use employee_info;
+create table emp (
+name VARCHAR (50),
+emp_id Int not null primary key ,
+salary decimal (10,2)
+);
+
+create table Customer(
+name VARCHAR (50),
+cust_id INT Not null unique primary KEY ,
+rate_score Decimal (10,2) not null 
+);
+
+create table Service (
+Service_name VARCHAR (50),
+address TEXT (100)
+);
+
+
+-- 2. ALTER--
+ Alter table Service 
+ add Feedback DECIMAL(10,2)
+ 
+
+
+
