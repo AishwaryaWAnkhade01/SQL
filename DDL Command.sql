b422b811af0e543f77199685781d461e35b9adf0
Aishwarya_09 <157670328+AishwaryaWAnkhade01@users.noreply.github.com>
2025-01-13 22:28:01 +0530

    Add files via upload

--- DDL Command.sql
+++ DDL Command.sql
@@ -1,29 +1,42 @@
---- -Data Definition Language--  
--- CREATE-- 
-Create database Employee_Info;
-use employee_info;
-create table emp (
-name VARCHAR (50),
-emp_id Int not null primary key ,
-salary decimal (10,2)
+-- --- -Data Definition Language--  
+-- -- CREATE-- 
+-- Create database Employee_Info;
+-- use employee_info;
+-- create table emp (
+-- name VARCHAR (50),
+-- emp_id Int not null primary key ,
+-- salary decimal (10,2)
+-- );
+
+-- create table Customer(
+-- name VARCHAR (50),
+-- cust_id INT Not null unique primary KEY ,
+-- rate_score Decimal (10,2) not null 
+-- );
+
+-- create table Service (
+-- Service_name VARCHAR (50),
+-- address TEXT (100)
+-- );
+
+
+-- -- 2. ALTER--
+--  Alter table Service 
+--  add Feedback DECIMAL(10,2)
+--  
+
+-- use enployee_info;
+-- select * from customer_service;
+
+create database CountryCode ;
+use CountryCode ;
+create table city(
+ID int,
+NAME VARCHAR(17),
+COUNTRYCODE BIGINT,
+DISTRICT VARCHAR(20),
+POPULATION int
 );
-
-create table Customer(
-name VARCHAR (50),
-cust_id INT Not null unique primary KEY ,
-rate_score Decimal (10,2) not null 
-);
-
-create table Service (
-Service_name VARCHAR (50),
-address TEXT (100)
-);
-
-
--- 2. ALTER--
- Alter table Service 
- add Feedback DECIMAL(10,2)
+select*from city;
  
-
-
 
